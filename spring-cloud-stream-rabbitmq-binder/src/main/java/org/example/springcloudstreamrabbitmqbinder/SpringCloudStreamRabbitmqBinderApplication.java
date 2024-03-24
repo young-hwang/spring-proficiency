8b3d4c8a6f65d584c46222757b57f430db96d894
young-hwang <ykcul02@gmail.com>
2024-03-24 20:52:28 +0900

    add: test-batch-exchange test 데이터 전달 추가

--- spring-cloud-stream-rabbitmq-binder/src/main/java/org/example/springcloudstreamrabbitmqbinder/SpringCloudStreamRabbitmqBinderApplication.java
+++ spring-cloud-stream-rabbitmq-binder/src/main/java/org/example/springcloudstreamrabbitmqbinder/SpringCloudStreamRabbitmqBinderApplication.java
@@ -1,13 +1,24 @@
 package org.example.springcloudstreamrabbitmqbinder;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringCloudStreamRabbitmqBinderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudStreamRabbitmqBinderApplication.class, args);
+	}
+
+	@Bean
+	public ApplicationRunner runner(RabbitTemplate template) {
+		return args -> {
+			template.convertAndSend("test-batch-exchange", "test-batch-key", "test-batch");
+			template.convertAndSend("test-batch-exchange","test-batch-key", "test-batch");
+		};
 	}
 
 }
