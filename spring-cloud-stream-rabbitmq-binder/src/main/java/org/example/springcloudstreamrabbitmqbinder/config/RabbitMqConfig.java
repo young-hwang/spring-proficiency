e9691c263d45f0abef3f161d1a731b5bfeda2ddb
young-hwang <ykcul02@gmail.com>
2024-03-24 20:35:46 +0900

    add: batch를 이용한 consume 처리

--- spring-cloud-stream-rabbitmq-binder/src/main/java/org/example/springcloudstreamrabbitmqbinder/config/RabbitMqConfig.java
+++ spring-cloud-stream-rabbitmq-binder/src/main/java/org/example/springcloudstreamrabbitmqbinder/config/RabbitMqConfig.java
@@ -3,6 +3,7 @@ package org.example.springcloudstreamrabbitmqbinder.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 @Configuration
@@ -10,5 +11,14 @@ public class RabbitMqConfig {
     @Bean
     public Consumer<String> testConsumer() {
         return v -> System.out.println("testConsumer: " + v);
+    }
+
+    /**
+     * inbound message를 조합하여 하나의 batch로 처리
+     * @return
+     */
+    @Bean
+    public Consumer<List<String>> testBatchConsumer() {
+        return list -> list.forEach(v -> System.out.println("testBatchConsumer: " + v));
     }
 }
