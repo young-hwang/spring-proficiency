8cb4f3c832de17da6f54664caa3374abe4e36ab9
young-hwang <ykcul02@gmail.com>
2024-04-01 20:04:48 +0900

    add: Redis Cache Manger

--- spring-data-redis/src/main/java/me/springdataredis/AppConfig.java
+++ spring-data-redis/src/main/java/me/springdataredis/AppConfig.java
@@ -2,6 +2,7 @@ package me.springdataredis;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -23,5 +24,11 @@ public class AppConfig {
                 .shutdownTimeout(Duration.ZERO)
                 .build();
         return new LettuceConnectionFactory(new RedisStandaloneConfiguration("localhost", 6379), clientConfig);
+    }
+
+    // RedisCacheManager is used to create a Redis cache manager
+    @Bean
+    public RedisCacheManager cacheManager() {
+        return RedisCacheManager.create(redisConnectionFactory());
     }
 }
