aa7a446d1a35986e3b56a5e3d13a87c6f47c25b7
young-hwang <ykcul02@gmail.com>
2024-04-30 02:05:20 +0900

    add: create sample for hyperloglog

---
+++ spring-data-redis-hyperloglog/src/main/java/me/springdataredishyperloglog/RedisConfiguration.java
@@ -0,0 +1,22 @@
+package me.springdataredishyperloglog;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@Configuration
+public class RedisConfiguration {
+    @Bean
+    LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
+        stringRedisTemplate.setConnectionFactory(redisConnectionFactory);
+        return stringRedisTemplate;
+    }
+}
---
+++ spring-data-redis-hyperloglog/src/test/java/me/springdataredishyperloglog/SpringDataRedisHyperloglogApplicationTests.java
@@ -0,0 +1,13 @@
+package me.springdataredishyperloglog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRedisHyperloglogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
