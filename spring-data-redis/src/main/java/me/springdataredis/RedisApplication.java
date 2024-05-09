b72c5a5bfa322c7e247a2de72ef10a2fa29c4d91
young-hwang <ykcul02@gmail.com>
2024-05-09 16:21:48 +0900

    fix: spring data redis config test

--- spring-data-redis/src/main/java/me/springdataredis/RedisApplication.java
+++ spring-data-redis/src/main/java/me/springdataredis/RedisApplication.java
@@ -8,7 +8,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.UnifiedJedis;
 
-import java.util.stream.IntStream;
+import java.util.List;
 
 @Slf4j
 public class RedisApplication {
@@ -29,21 +29,28 @@ public class RedisApplication {
 
 		template.opsForValue().set("foo", "bar");
 
-        System.out.println(System.currentTimeMillis());
-//        JedisPool pool = new JedisPool("localhost", 6379);
-//            pool.get
         Jedis jedis = (Jedis) connectionFactory.getConnection().getNativeConnection();
-//        UnifiedJedis pool = new UnifiedJedis(HostAndPort.from("localhost:6379"));
         UnifiedJedis pool = new UnifiedJedis(jedis.getConnection());
-        IntStream.range(1, 990000).forEach(i -> {
-            System.out.println(String.valueOf(i));
-            pool.sadd("resource:set", "$2b$05$bFJ.EtW3cE5Lm.whhm81uOx0lYuKzos4OmYsC5pv0A6cel.LO4xMO^#1U8389ATPE/szowZGlK27A==:http://www.google.com" + i);
-        });
+        // Count-Min Sketch 생성
+        pool.cmsInitByProb("cms:heatmap", 0.008d, 0.001d);
+
+        // Count-Min Sketch에 데이터 추가
+        pool.cmsIncrBy("cms:heatmap", "202405040000:0", 1);
+
+        // Count-Min Sketch에 데이터 조회
+        List<Long> counts = pool.cmsQuery("cms:heatmap", "202405040000:0");
+        counts.forEach(System.out::println);
+
+
+//        IntStream.range(1, 990000).forEach(i -> {
+//            System.out.println(String.valueOf(i));
+//            pool.sadd("resource:set", "$2b$05$bFJ.EtW3cE5Lm.whhm81uOx0lYuKzos4OmYsC5pv0A6cel.LO4xMO^#1U8389ATPE/szowZGlK27A==:http://www.google.com" + i);
+//        });
 //        pool.bfReserve("resource:filter", 0.00001, 1000000);
 //        pool.bfAdd("resource:filter", "$2b$05$bFJ.EtW3cE5Lm.whhm81uOx0lYuKzos4OmYsC5pv0A6cel.LO4xMO^#1U8389ATPE/szowZGlK27A==:http://www.google.com");
 //        boolean b = pool.bfExists("resource:filter", "$2b$05$bFJ.EtW3cE5Lm.whhm81uOx0lYuKzos4OmYsC5pv0A6cel.LO4xMO^#1U8389ATPE/szowZGlK27A==:http://www.google.com");
-        pool.close();
-        System.out.println(System.currentTimeMillis());
+//        pool.close();
+//        System.out.println(System.currentTimeMillis());
 //        System.out.println("result " + b);
 
 //        try (Jedis jedis = pool.getResource()) {
