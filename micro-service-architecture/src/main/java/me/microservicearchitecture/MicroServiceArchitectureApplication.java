e776e7cb45415351a59272676f5d978c874b7ed4
young-hwang <ykcul02@gmail.com>
2024-05-11 21:11:31 +0900

    add: eureka discovery server

---
+++ micro-service-architecture/src/main/java/me/microservicearchitecture/MicroServiceArchitectureApplication.java
@@ -0,0 +1,13 @@
+package me.microservicearchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceArchitectureApplication.class, args);
+    }
+
+}
---
+++ micro-service-architecture/src/test/java/me/microservicearchitecture/MicroServiceArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package me.microservicearchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
