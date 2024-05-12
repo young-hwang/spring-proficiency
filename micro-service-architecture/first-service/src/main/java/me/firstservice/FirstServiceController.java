6c81a5cefaa4b75accccaf0ce239c95157550c04
young-hwang <ykcul02@gmail.com>
2024-05-12 22:10:41 +0900

    add: micro service server

---
+++ micro-service-architecture/first-service/src/main/java/me/firstservice/FirstServiceController.java
@@ -0,0 +1,14 @@
+package me.firstservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/first-service")
+@RestController
+public class FirstServiceController {
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the first service!";
+    }
+}
