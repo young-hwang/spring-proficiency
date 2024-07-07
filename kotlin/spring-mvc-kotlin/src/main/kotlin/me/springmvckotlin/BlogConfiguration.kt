cc37bfb70217509bf3782f77976378089e1bf7ac
young-hwang <ykcul02@gmail.com>
2024-07-07 23:08:44 +0900

    add: api test

---
+++ kotlin/spring-mvc-kotlin/src/main/kotlin/me/springmvckotlin/BlogConfiguration.kt
@@ -0,0 +1,27 @@
+package me.springmvckotlin
+
+import org.springframework.boot.ApplicationRunner
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class BlogConfiguration {
+
+    @Bean
+    fun databaseInitializer(userRepository: UserRepository, articleRepository: ArticleRepository) = ApplicationRunner {
+        val johnDoe = userRepository.save(User("johndoe", "John", "Doe"))
+        articleRepository.save(Article(
+            title = "Lorem",
+            headline = "Lorem",
+            content = "dolor sit amet",
+            author = johnDoe
+        ))
+        articleRepository.save(Article(
+            title = "Ipsum",
+            headline = "Ipsum",
+            content = "dolor sit amet",
+            author = johnDoe
+        ))
+    }
+
+}
--- kotlin/spring-mvc-kotlin/src/test/kotlin/me/springmvckotlin/IntegrationTests.kt
+++ kotlin/spring-mvc-kotlin/src/test/kotlin/me/springmvckotlin/IntegrationTests.kt
@@ -19,14 +19,19 @@ class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
 
     @Test
     fun `Assert blog page title, content and status code`() {
+        println(">> Assert blog page title, content and status code")
         val entity = restTemplate.getForEntity<String>("/")
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-        assertThat(entity.body).contains("<h1>Blog</h1>")
+        assertThat(entity.body).contains("<h1>Blog</h1>", "Lorem")
     }
 
     @Test
     fun `Assert article page title, content and status code`() {
-        println(">> TODO")
+        println(">> Assert article page title, content and status code")
+        val title = "Lorem"
+        val entity = restTemplate.getForEntity<String>("/article/${title.toSlug()}")
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).contains(title, "Lorem", "dolor sit amet")
     }
 
     @AfterAll
