68e09da5d9fedf529c61d69c1217e45b5a16952d
young-hwang <ykcul02@gmail.com>
2024-07-29 15:25:26 +0900

    add: spring ssh webclient

---
+++ spring-ssh-webclient/src/main/java/me/springsshwebclient/SpringSshWebclientApplication.java
@@ -0,0 +1,13 @@
+package me.springsshwebclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSshWebclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSshWebclientApplication.class, args);
+	}
+
+}
---
+++ spring-ssh-webclient/src/test/java/me/springsshwebclient/SpringSshWebclientApplicationTests.java
@@ -0,0 +1,13 @@
+package me.springsshwebclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSshWebclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
