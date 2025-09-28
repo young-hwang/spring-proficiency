023acc97955d8ee05dbe1cb439abe040d7f914bd
young-hwang <younghwang@micube.co.kr>
2025-09-29 00:45:51 +0900

    Fix inconsistent indentation in `AiServiceDefaultMethod` class to improve code readability.

--- spring-ai/ch03-prompt/src/main/java/me/service/AiServiceDefaultMethod.java
+++ spring-ai/ch03-prompt/src/main/java/me/service/AiServiceDefaultMethod.java
@@ -10,26 +10,26 @@ import reactor.core.publisher.Flux;
 @Service
 @Slf4j
 public class AiServiceDefaultMethod {
-  // ##### 필드 #####
-  private ChatClient chatClient;
+    // ##### 필드 #####
+    private ChatClient chatClient;
 
-  // ##### 생성자 #####
-  public AiServiceDefaultMethod(ChatClient.Builder chatClientBuilder) {
-    chatClient = chatClientBuilder
-        .defaultSystem("적절한 감탄사, 웃음등을 넣어서 친절하게 대화를 해주세요.")
-        .defaultOptions(ChatOptions.builder()
-            .temperature(1.0)
-            .maxTokens(300)
-            .build())
-        .build();
-  }
+    // ##### 생성자 #####
+    public AiServiceDefaultMethod(ChatClient.Builder chatClientBuilder) {
+        chatClient = chatClientBuilder
+                .defaultSystem("적절한 감탄사, 웃음등을 넣어서 친절하게 대화를 해주세요.")
+                .defaultOptions(ChatOptions.builder()
+                        .temperature(1.0)
+                        .maxTokens(300)
+                        .build())
+                .build();
+    }
 
-  // ##### 메소드 #####
-  public Flux<String> defaultMethod(String question) {   
-    Flux<String> response = chatClient.prompt()
-        .user(question)
-        .stream()
-        .content();
-    return response;
-  }
+    // ##### 메소드 #####
+    public Flux<String> defaultMethod(String question) {
+        Flux<String> response = chatClient.prompt()
+                .user(question)
+                .stream()
+                .content();
+        return response;
+    }
 }
